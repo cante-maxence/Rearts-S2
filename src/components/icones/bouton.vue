96dacfec0ec6fbd7ffdb12a21866696da43bbdbf
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-05-24 15:44:24 +0200

    problemes tailwind

--- src/components/icones/bouton.vue
+++ src/components/icones/bouton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="bg-rouge-primary text-blanc-primary font-raleway font-normal  py-3 px-5 rounded-3xl">
+  <button class="bg-Rouge text-blanc-primary font-raleway font-normal  py-3 px-5 rounded-3xl">
         <slot/>
   </button>
 
