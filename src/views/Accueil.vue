51f80d64f155438b96b48c68d1a7e0580a3d237f
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-06-01 22:25:06 +0200

    ajout contenu page d'accueil

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,23 +1,85 @@
 <template>
 
-<h2>Découvrez réarts !</h2>
-<p>
-    Un site sur lequel l’imagination d’un enfant ce rencontre avec les capacitées d’un artiste !
-</p>
-<p class="font-bold text-Rouge">
-    Un petit dessin pour lui, un grand souvenir pour vous !
-</p>
-<Bouton>
-    <slot>S'inscrire</slot>
-</Bouton>
+    <h2>Découvrez réarts !</h2>
+    <p>
+        Un site sur lequel l’imagination d’un enfant ce rencontre avec les capacitées d’un artiste !
+    </p>
+    <p class="font-bold text-Rouge">
+        Un petit dessin pour lui, un grand souvenir pour vous !
+    </p>
+    <Bouton>
+        <slot>S'inscrire</slot>
+    </Bouton>
 
-<h3>
-    Les tendances !
-</h3>
+    <h3>
+        Les tendances !
+    </h3>
 
+            <!-- caroussel -->
+    <div class="relative rounded-xl overflow-auto">
+    <!-- Contents -->
+    <div class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-3 pt-3">
+        <div class="snap-center shrink-0">
+        <div class="shrink-0 w-4 sm:w-48"></div>
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image1.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image2.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image3.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image4.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image5.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image6.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image7.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/image8.jpg">
+        </div>
+        <div class="snap-center shrink-0 first:pl-8 last:pr-8">
+        <img class="shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white" src="/images-caroussel-hp/Rectangle9.jpg">
+        </div>
+        <div class="snap-center shrink-0">
+        <div class="shrink-0 w-4 sm:w-48"></div>
+        </div>
+    </div>
+    </div>
+            <!-- caroussel -->
 
+    <Bouton>
+        <slot>Découvrir</slot>
+    </Bouton>
 
+    <h3>
+        N’oubliez pas les associations !
+    </h3>
+    <p>
+        Pour chaque dessins n’oubliez de sélectionnez une association partenaires.
+    </p>
+    <img class="shrink-0 w-28 h-28 shadow-xl bg-white" src="/images-hp/Association1.jpg">
+    <img class="shrink-0 w-28 h-28 shadow-xl bg-white" src="/images-hp/Association2.jpg">
 
+    <Bouton>
+        <slot>Voir plus</slot>
+    </Bouton>
+
+    <h3>
+        Des vidéos youtube !
+    </h3>
+    <p>
+        Venez découvrir les différents artistes de Réarts sur notre chaînes Youtube ! 
+    </p>
+    <iframe width="auto" height="200" src="https://www.youtube.com/embed/uAPNFdrLINM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
 
